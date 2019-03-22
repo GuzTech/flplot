84f523fa209f0e60c07aa736bca4a7866225cd13
Oguz Meteer <info@guztech.nl>
2019-03-22 17:45:36 +0100

    Implemented zooming with mouse wheel
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- PlotWidget.cpp
+++ PlotWidget.cpp
@@ -12,6 +12,7 @@
 #include <cstdlib>
 #include <memory>
 #include <string>
+#include <iostream>
 
 PlotWidget::PlotWidget(const int x, const int y, const int w, const int h, const std::string cap)
 	: Fl_Widget(x, y, w, h, cap.c_str())
@@ -70,24 +71,25 @@ PlotWidget::dtipvec::iterator PlotWidget::getClosestDt(const int mx, const int m
 
 int PlotWidget::handle(int event)
 {
-	int mx = Fl::event_x(),
-			my = Fl::event_y();
-	double wd = limc.xr - limc.xl,
-			ht = limc.yr - limc.yl;
+	const int mx = Fl::event_x();
+	const int my = Fl::event_y();
+	const double wd = limc.xr - limc.xl;
+	const double ht = limc.yr - limc.yl;
+
 	switch(event) {
 	case FL_PUSH:
-		cdtip = getClosestDt(mx,my,wd,ht);
+		cdtip = getClosestDt(mx, my, wd, ht);
 		if(Fl::event_button() == FL_LEFT_MOUSE) {
 			//First check if user clicked on a data-tip
 			//Find a data-tip point within a neighborhood of the mouse pointer
 			if(cdtip == datatips.end()) {
 				//User didn't click on an existing datatip
 				datatip tmp;
-				if(getClosestPt(tmp,mx,my,wd,ht)) {
+				if(getClosestPt(tmp, mx, my, wd, ht)) {
 					tmp.lx = 10;
 					tmp.ly = -10;
 					datatips.push_back(tmp);
-					cdtip = datatips.end()-1;
+					cdtip = datatips.end() - 1;
 				} else {
 					zooming = true;
 					zoomx = mx;
@@ -159,35 +161,58 @@ int PlotWidget::handle(int event)
 	case FL_RELEASE:
 		if(Fl::event_button() == FL_LEFT_MOUSE) {
 			if(zooming) {
-				int mx = Fl::event_x(),
-						my = Fl::event_y();
-				int dx = mx-zoomx, dy = my-zoomy;
+				int dx = mx - zoomx;
+				int dy = my - zoomy;
 				zooming = false;
 
 				//Get rect (lul)
-				zoomx = std::min(zoomx,zoomx+dx);
-				zoomy = std::min(zoomy,zoomy+dy);
+				zoomx = std::min(zoomx, mx);
+				zoomy = std::min(zoomy, my);
 				dx = abs(dx);
 				dy = abs(dy);
 
 				if(dx > 2 || dy > 2) {
 					//Clip
-					zoomx = std::max(zoomx,x());
-					zoomy = std::max(zoomy,y());
-					dx = std::min(dx,w()+x()-zoomx);
-					dy = std::min(dy,h()+y()-zoomy);
+					zoomx = std::max(zoomx, x());
+					zoomy = std::max(zoomy, y());
+					dx = std::min(dx, w() + x() - zoomx);
+					dy = std::min(dy, h() + y() - zoomy);
 
 					//Scale+shift
-					limc.xl = ((double)(zoomx-x())/w())*wd + limc.xl;
-					limc.xr = limc.xl + ((double)dx/w())*wd;
-					limc.yl = ((double)(y()+h()-dy-zoomy)/h())*ht + limc.yl;
-					limc.yr = limc.yl + ((double)dy/h())*ht;
+					limc.xl = ((double)(zoomx - x()) / w()) * wd + limc.xl;
+					limc.xr = limc.xl + ((double)dx / w()) * wd;
+					limc.yl = ((double)(y() + h() - dy - zoomy) / h()) * ht + limc.yl;
+					limc.yr = limc.yl + ((double)dy / h()) * ht;
 				}
 				redraw();
 			}
 		}
 		//cout << "Release at " << Fl::event_x() << " " << Fl::event_y() <<endl;
 		return 1;
+	case FL_MOUSEWHEEL:
+	{
+		// Fl::event_dx() gives horizontal scrolling values.
+		// Fl::event_dy() gives vertical scrolling values. -1 is zooming in, 1 is zooming out.
+		double coefficient = Fl::event_dy() * -0.1;
+
+		// mx, my are in window space
+		// x(), y() are in plot widget coordinates
+		// w(), h() are the absolute widget sizes
+		const double mxx = ((double)(mx - x())) / w() * wd;
+		const double myy = ((double)(h() + y() - my)) / h() * ht;
+
+		const double dxr = wd - mxx;
+		const double dyr = ht - myy;
+
+		limc.xl = (mxx * coefficient) + limc.xl;
+		limc.xr = limc.xr - (dxr * coefficient);
+		limc.yl = (myy * coefficient) + limc.yl;
+		limc.yr = limc.yr - (dyr * coefficient);
+
+		redraw();
+
+		return 1;
+	}
 	default:
 		return Fl_Widget::handle(event);
 	}
@@ -195,8 +220,8 @@ int PlotWidget::handle(int event)
 }
 void PlotWidget::draw()
 {
-	double wd = limc.xr - limc.xl,
-			ht = limc.yr - limc.yl;
+	const double wd = limc.xr - limc.xl;
+	const double ht = limc.yr - limc.yl;
 	char str[50];
 
 	//Background
